e193d6c9897e815f888224682b343e556bed5395
dembasow98 <dastech1998@gmail.com>
2022-09-07 16:03:48 +0300

    Initial setup for the backend part of the application and database connection.

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+//configuring dotenv
+dotenv.config();
+
+//creating express app
+const app = express();
+
+//Allowing app to use json
+app.use(express.json());
+
+//Allowing app to use cors
+app.use(cors());
+
+//variables
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+//Connecting to database
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("Database connected successfully!"))
+.then(() => app.listen(PORT, () => console.log(`Server running on PORT: ${PORT}`)))
+.catch((error) => console.log(error.message));
